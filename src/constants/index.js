2efc654504ce6315f71b1efa08f527248bd27805
Jaslenek <imjaslene.minhas@gmail.com>
2026-03-26 21:15:45 +0530

    resume added

--- src/constants/index.js
+++ src/constants/index.js
@@ -173,12 +173,12 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/Jaslenek',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/jaslene-kaur-3b151328b/',
     }
 ];
 
